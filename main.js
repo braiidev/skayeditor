4ffea2679d59dde6078cd49ba3ec088130570390
braianleonardocano@gmail.com <braianleonardocano@gmail.com>
2023-03-16 15:11:41 -0300

    add menu

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const $ = (s) => document.querySelector(s);
 const $a = $("#app");
+const $t = $("#name");
 const $h = $("#html");
 const $c = $("#css");
 const $j = $("#js");
@@ -8,37 +9,44 @@ let u = "",
   h = "",
   c = "",
   j = "",
+  t = "",
   hc = "";
 const _e = (e) => window.btoa(e);
 const _d = (d) => window.atob(d);
-const _r = (r) => history.replaceState(null, null, `/#${r}`);
+const _r = (r) => history.replaceState(null, null, `/skayeditor/#${r}`);
 
 function i() {
   const { hash: p } = window.location;
-  const [_h, _c, _j] = p.slice(1).split("%7C");
+  const [_h, _c, _j, _t] = p.slice(1).split("%7C");
   h = _d(_h);
   c = _d(_c);
   j = _d(_j);
+  t = _d(_t);
   $h.value = h;
   $c.value = c;
   $j.value = j;
-  $p.srcdoc = __h(h, c, j);
+  $t.value = t;
+  $p.srcdoc = __h(h, c, j, t);
 }
 $a.addEventListener("input", (e) => {
   const { id: i, value: v } = e.target;
+  t = $t.value;
   if (i === "html") h = v;
   if (i === "css") c = v;
   if (i === "js") j = v;
-  hc = h != "" || c != "" || j != "" ? `${_e(h)}%7C${_e(c)}%7C${_e(j)}` : "";
+  hc =
+    h != "" || c != "" || j != ""
+      ? `${_e(h)}%7C${_e(c)}%7C${_e(j)}%7C${_e(t)}`
+      : "";
   _r(hc);
-  $p.srcdoc = __h(h, c, j);
+  $p.srcdoc = __h(h, c, j, t);
 });
-const __h = (h, c, j) => {
+const __h = (h, c, j, t) => {
   return `
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Skay online-editor</title>
+    <title>${t || "Skay editor"}</title>
     <style>${c}</style>
 </head>
 <body>
