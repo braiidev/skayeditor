b3157de75d5936eef63dc6a576fdc8029f01fdf9
braianleonardocano@gmail.com <braianleonardocano@gmail.com>
2023-03-17 17:54:52 -0300

    copy to clipboard

--- func.js
+++ func.js
@@ -6,7 +6,15 @@ btn.addEventListener("click", (e) => _t(btn, m));
 m.addEventListener("click", (e) => _ev(e));
 function _ev(e) {
   const { target: t } = e;
-  t.id == "dm" ? _dm(t.checked) : t.id == "size" ? _rf(t.value) : _t(btn, m);
+  t.id == "dm"
+    ? _dm(t.checked)
+    : t.id == "size"
+    ? _rf(t.value)
+    : t.id == "reset"
+    ? _rst("#name", "#html", "#css", "#js")
+    : (t.id = "copy")
+    ? _c(window.location.href)
+    : _t(btn, m);
 }
 function _dm(v) {
   if (v) {
@@ -31,3 +39,20 @@ function _t(...l) {
     _l.classList.toggle("opened");
   });
 }
+function _rst(...l) {
+  l.forEach((_l) => {
+    $(_l).value = "";
+  });
+}
+
+const _c = (str) => {
+  if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+    const t = $("#copy");
+    t.className = "success";
+    setInterval(function () {
+      t.className = "";
+    }, 3000);
+    return navigator.clipboard.writeText(str);
+  }
+  return Promise.reject("The Clipboard API is not available.");
+};
