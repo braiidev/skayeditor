6c49b18a0f07333597f9363035484549283a9b18
Braian L. Cano <braianleonardocano@gmail.com>
2025-01-02 22:39:38 -0300

    Add CodeMirror at my Editor

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,24 +9,64 @@ addEventListener("DOMContentLoaded", () => {
     i.addEventListener("click", () => menuHandler(i));
   });
 
-  const $html = document.querySelector("#html>textarea");
-  const $css = document.querySelector("#css>textarea");
-  const $js = document.querySelector("#js>textarea");
-
-  const $iframe = document.querySelector("#preview>iframe");
-  [$html, $css, $js].forEach(($e) =>
-    $e.addEventListener("input", () => {
-      let rendering =
-        $html.value +
-        "<style>" +
-        $css.value +
-        "</style>" +
-        "<scri" +
-        "pt>" +
-        $js.value +
-        "</scri" +
-        "pt>";
-      $iframe.setAttribute("srcdoc", rendering);
-    })
-  );
+  //const $html = document.querySelector("#html>textarea");
+  //const $css = document.querySelector("#css>textarea");
+  //const $js = document.querySelector("#js>textarea");
+  //
+  //const $iframe = document.querySelector("#preview>iframe");
+  //[$html, $css, $js].forEach(($e) =>
+  //  $e.addEventListener("input", () => {
+  //    let rendering =
+  //      $html.value +
+  //      "<style>" +
+  //      $css.value +
+  //      "</style>" +
+  //      "<scri" +
+  //      "pt>" +
+  //      $js.value +
+  //      "</scri" +
+  //      "pt>";
+  //    $iframe.setAttribute("srcdoc", rendering);
+  //  })
+  //);
 });
+
+
+const htmlEditor = CodeMirror.fromTextArea(document.getElementById("html-e"), {
+      mode: "html",
+      theme: "darcula",
+      lineNumbers: true,
+      autoCloseTags: true,
+    });
+
+    const cssEditor = CodeMirror.fromTextArea(document.getElementById("css-e"), {
+      mode: "css",
+      theme: "darcula",
+      lineNumbers: true,
+      autoCloseBrackets: true,
+    });
+
+    const jsEditor = CodeMirror.fromTextArea(document.getElementById("js-e"), {
+      mode: "javascript",
+      theme: "darcula",
+      lineNumbers: true,
+      autoCloseBrackets: true,
+    });
+
+    const previewFrame = document.getElementById("preview-e");
+    function updatePreview() {
+      const html = htmlEditor.getValue();
+      const css = `<style>${cssEditor.getValue()}</style>`;
+      const js = `<script>${jsEditor.getValue()}<\/script>`;
+      const previewContent = `${html}${css}${js}`;
+
+      const doc = previewFrame.contentDocument || previewFrame.contentWindow.document;
+      doc.open();
+      doc.write(previewContent);
+      doc.close();
+    }
+
+    htmlEditor.on("change", updatePreview);
+    cssEditor.on("change", updatePreview);
+    jsEditor.on("change", updatePreview);
+
